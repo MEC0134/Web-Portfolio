fb2541ce70915854263f6efef44f9d4224fa984b
M. Enes Celik <m96celik@gmail.com>
2023-10-02 15:27:26 +0300

    jquery-file-removed

--- app.js
+++ app.js
@@ -5,10 +5,8 @@ const pug = require('pug');
 const bodyParser = require('body-parser');
 
 
-// Require JSON File
 const myProjects = require('./data.json');
 
-// Assign app to express 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -26,33 +24,23 @@ app.post('/filterProjs', (req, res) => {
     const selectedCategory = req.body.category;
 
     if (selectedCategory === 'all') {
-        // Redirect to the home route
         res.redirect('/');
     } else {
-        // Filter the projects based on the selected category
         const projectsToSend = myProjects.Projects.filter(
             project => project.category === selectedCategory
         );
-
-        // Render the home route with the filtered projects
         res.render('index', { projects: { Projects: projectsToSend } });
     }
 });
 
-// home route
 app.get('/', (req, res) => {
     res.render('index', { projects: myProjects });
 });
 
-
-
-// About page 
 app.get('/about', (req, res) => {
     res.render('about');
 });
 
-
-// Project
 app.get('/project/:id', (req, res) => {
     const projectId = req.params.id;
     const project = myProjects.Projects.find(p => p.id === projectId);
@@ -60,8 +48,6 @@ app.get('/project/:id', (req, res) => {
     res.render('project', { project });
 });
 
-
-// Configure Server for Heroku and localy
 app.listen(process.env.PORT || 3000, () => {
     console.log("Server running on 3000");
 });
